33480bdcdbd4fdaf6bc38596d709f2ed3ef0d46a
kyleaw25 <kyleaw25@gmail.com>
2025-12-09 03:40:44 +0000

    Update index.py

--- index.py
+++ index.py
@@ -4,16 +4,17 @@ import numpy as np
 # load the model
 model = joblib.load("exoplanetPrediction_model.joblib")
 
-#star to predict
+#star to predict, edit values to recieve prediction!
 sample = np.array([[  
-    34153,    # star_temp
-    1.3155,     # star_radius
-    2.2923,     # star_mass
-    3.46,     # star_metallicity
-    4.91      # star_surfaceGravity
+    4800,    # star_temp
+    1.0,     # star_radius
+    1.0,     # star_mass
+    1.0,     # star_metallicity
+    1.0      # star_surfaceGravity
 ]])
 
 prediction = model.predict(sample)
 prediction = max(0, prediction[0])  # avoid negatives
 
 print("Predicted planets:", round(prediction))
+
